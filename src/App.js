0ae8ac30715a77372ed59712a2172ca93235a0a5
sayeed <sayeed0209@gmail.com>
2022-06-26 20:21:00 +0200

    edit todo and check todo functionalities

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { useState, useEffect } from 'react';
+import List from './components/List/List';
+import Alert from './components/Alert/Alert';
+const App = () => {
+	const [todo, setTodo] = useState('');
+	const [todoList, setTodoList] = useState([]);
+	const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+	const [isEditing, setIsEditing] = useState(false);
+	const [editId, setEditId] = useState(null);
+	const submitHandler = e => {
+		e.preventDefault();
+	
+		if (todo.length === 0 || todo.trim().length < 0) {
+			setAlert({ show: true, msg: 'please enter a todo', type: 'danger' });
+		} else if (todo && isEditing) {
+			// will edit
+			const updateTodo = todoList.map(todoItem => {
+				if (todoItem.id === editId) {
+					return { ...todoItem, title: todo };
+				}
+				return todoItem;
+			});
+			setTodoList(updateTodo);
+			setTodo('');
+			setEditId(null);
+			setIsEditing(false);
+			setAlert({ show: true, type: 'success', msg: 'Updated Successfully' });
+		} else {
+			const newTodo = {
+				id: new Date().getTime().toString(),
+				title: todo,
+				completed: false,
+			};
+			setTodoList([...todoList, newTodo]);
+			setAlert({ show: true, msg: 'Todo added successfully', type: 'success' });
+			setTodo('');
+		}
+	};
+	const hideAlert = () => {
+		setAlert({ show: false });
+	};
+	const editTodo = id => {
+		const todoItem = todoList.find(item => item.id === id);
+		setIsEditing(true);
+		setEditId(id);
+		setTodo(todoItem.title);
+	};
+	const handleChnageCheckbox = id => {
+		const newTodo = todoList.map(todoItem => {
+			if (todoItem.id === id) {
+				return { ...todoItem, completed: !todoItem.completed };
+			}
+			return todoItem;
+		});
+		setTodoList(newTodo);
+	};
+	
+	return (
+		<main>
+			<section className="section-center">
+				<h1>todo list</h1>
+				<form onSubmit={submitHandler}>
+					{alert.show && <Alert {...alert} hideAlert={hideAlert} />}
+					<div className="form-control">
+						<input
+							type="text"
+							name="todo"
+							id="todo"
+							className="todoInput"
+							placeholder="Add todo"
+							onChange={e => setTodo(e.target.value)}
+							value={todo}
+						/>
+						<button className="submit-btn">
+							{isEditing ? 'edit' : 'addtodo'}
+						</button>
+					</div>
+				</form>
+				{todoList.length > 0 && (
+					<div>
+						<List
+							todoList={todoList}
+							editTodo={editTodo}
+							handleChnage={handleChnageCheckbox}
+						/>
+						<button className="clear-btn">clear all</button>
+					</div>
+				)}
+			</section>
+		</main>
+	);
+};
 
 export default App;
---
+++ src/components/List/List.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { FaEdit, FaTrash } from 'react-icons/fa';
+const List = ({ todoList, editTodo, handleChnage }) => {
+	return (
+		<section>
+			{todoList.map(todo => {
+				const { id, title } = todo;
+				return (
+					<ul className="todolist-container" key={id}>
+						<li>
+							<input
+								type="checkbox"
+								checked={todo.completed}
+								onChange={() => {
+									handleChnage(id);
+								}}
+								
+							/>
+						</li>
+						<li
+							className="todo-item"
+							style={{ textDecoration: todo.completed ? 'line-through' : '' }}
+						>
+							{title}
+						</li>
+						<li className="btn-container">
+							<button className="edit-btn">
+								<FaEdit className="icon" onClick={() => editTodo(id)} />
+							</button>
+							<button className="delete-btn">
+								<FaTrash className="icon" />
+							</button>
+						</li>
+					</ul>
+				);
+			})}
+		</section>
+	);
+};
+
+export default List;
